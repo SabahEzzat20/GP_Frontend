246fdd5f939ec17533c9ab0e9c1b38294220c007
SaraMohamedKamal <sarah.mohamed2070@gmail.com>
2024-02-09 23:02:22 +0200

    register not completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from 'react-router-dom';
+//import { Outlet } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { DashboardSidebar } from './AdminModule/DashboardSidebar.jsx';
+//import { DashboardSidebar } from './AdminModule/DashboardSidebar.jsx';
 //import LoginForm from './Shared/LoginForm';
-
+import Register from './Shared/Register.jsx';
 //import Navbar from "./DoctorModule/Navbar";
 
 export const App = () => {
   return (
     <div className="App">
-     <DashboardSidebar />
-     <Outlet />
+        <Register />
     </div>
   );
 };
