437572ac29f46c51bbd91731047c80984ca87bf6
SabahHassan692 <103220823+SabahHassan692@users.noreply.github.com>
2024-02-09 13:09:45 +0200

    app.js changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import Sidebar from './AdminModule/Sidebar';
 import UpperNav from './DoctorModule/UpperNav';
 import { role } from './AdminModule/Sidebar';
 export const App = () => {
-<<<<<<< HEAD
-    return ( <
-        div className = "App" > { /* <DragDrop /> */ } {
-            /* <Sidebar />
-                  <Outlet /> */
-        } <
-        Navbar / >
-        <
-        /div>
-    );
-}
-=======
   return (
     <div className="App">
       <Sidebar />
@@ -32,4 +20,3 @@ export const App = () => {
     </div>
   );
 }
->>>>>>> 13075eae4378e4e8533c66ad9a679f68ff8f03ea
