08082984fd7edb9ef6d135ebe17ffed707617afb
SaraMohamedKamal <sarah.mohamed2070@gmail.com>
2024-03-05 13:10:50 +0200

    full-calendar

--- src/AdminModule/FullCalender.jsx
+++ src/AdminModule/FullCalender.jsx
@@ -1,30 +1,148 @@
-import React from "react";
+//  import React from "react";
+//   import "../Sass/FullCalender.css";
+//  import Fullcalendar from "@fullcalendar/react";
+//  import dayGridPlugin from "@fullcalendar/daygrid";
+//  import timeGridPlugin from "@fullcalendar/timegrid";
+//  import interactionPlugin from "@fullcalendar/interaction";
+// // // //  npm i @fullcalendar/daygrid @fullcalendar/interaction @fullcalendar/timegrid
+
+//  function FullCalender(){
+//      return(
+//          <div className="full-calender">
+//        <Fullcalendar
+//          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+//          initialView={"timeGridWeek"}
+//          // weekends={false}
+//          headerToolbar={{
+//            start: "today prev,next",
+//            center: "title",
+//            end: "dayGridMonth,timeGridWeek,timeGridDay",
+//          }}
+//          height={"90vh"}
+//        />
+//      </div>
+//      )
+//  }
+
+//  export default FullCalender;
+
+// import React, { useEffect, useRef } from 'react';
+// import FullCalendar from '@fullcalendar/react';
+// // import dayGridPlugin from '@fullcalendar/daygrid';
+
+// const FullCalender = () => {
+//   const calendarRef = useRef(null);
+
+//   useEffect(() => {
+//     const handleDOMContentLoaded = () => {
+//       const calendarEl = calendarRef.current;
+//       const calendar = new FullCalendar.Calendar(calendarEl, {
+//         initialView: 'dayGridMonth',
+//         headerToolbar: {
+//           center: 'addEventButton'
+//         },
+//         customButtons: {
+//           addEventButton: {
+//             text: 'add event...',
+//             click: function() {
+//               var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+//               var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+
+//               if (!isNaN(date.valueOf())) { // valid?
+//                 calendar.addEvent({
+//                   title: 'dynamic event',
+//                   start: date,
+//                   allDay: true
+//                 });
+//                 alert('Great. Now, update your database...');
+//               } else {
+//                 alert('Invalid date.');
+//               }
+//             }
+//           }
+//         }
+//       });
+
+//       calendar.render();
+//     };
+
+//     document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
+
+//     return () => {
+//       document.removeEventListener('DOMContentLoaded', handleDOMContentLoaded);
+//     };
+//   }, []);
+
+//   return <div ref={calendarRef} />;
+// };
+
+// export default FullCalender;
+
+import React, { useEffect, useState } from "react";
 import "../Sass/FullCalender.css";
-import Fullcalendar from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-//  npm i @fullcalendar/daygrid @fullcalendar/interaction @fullcalendar/timegrid 
 
-function FullCalender(){
-    return(
-        <div className="full-calender">
-      <Fullcalendar
+const data = [
+  {
+    year: 2024,
+    month: "03",
+    day: "11",
+    time: "08:00",
+  },
+  {
+    year: 2024,
+    month: "03",
+    day: "12",
+    time: "10:00",
+  },
+  {
+    year: 2024,
+    month: "03",
+    day: "08",
+    time: "11:00",
+  },
+];
+
+function FullCalender() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const formattedEvents = data.map((event) => {
+      const date = new Date(
+        `${event.year}-${event.month}-${event.day}T${event.time}`
+      );
+
+      return {
+        title: "An Appointment",
+        start: date,
+        allDay: false,
+      };
+    });
+
+    setEvents(formattedEvents);
+  }, []);
+
+  return (
+    <div className="full-calendar">
+      <div className="actual-calendar">
+
+      <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"timeGridWeek"}
-        // weekends={false}
         headerToolbar={{
-          start: "today prev,next", 
+          start: "today prev,next",
           center: "title",
-          end: "dayGridMonth,timeGridWeek,timeGridDay", 
+          end: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
-        height={"90vh"}
+        height={"80vh"}
+        events={events}
       />
+      </div>
     </div>
-    )
+  );
 }
 
 export default FullCalender;
-
-
-
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import  App  from "./App";
+import App from "./App";
 import ViewPatients from "./AdminModule/ViewPatients";
 import ViewDoctors from "./AdminModule/ViewDoctors";
 import ViewDoctorAppointments from "./DoctorModule/ViewDoctorAppointments";
@@ -10,59 +10,55 @@ import HomePage from "./PatientsModule/HomePage";
 import HomePage2 from "./PatientsModule/HomePage2";
 import ContactUs from "./PatientsModule/ContactUs";
 import FullCalender from "./AdminModule/FullCalender";
-import LoginForm from "./shared/LoginForm";
-import Register from './shared/Register';
 // import Test from './AdminModule/Test';
-export const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/viewPatients",
-        element: <ViewPatients />,
-      },
-      {
-        path: "/viewDoctors",
-        element: <ViewDoctors />,
-      },
-      {
-        path: "/fullCalender",
-        element: <FullCalender />,
-      },
-      {
-        path: "/viewDoctorAppointments",
-        element: <ViewDoctorAppointments />,
-      },
-      {
-        path: "/calendar",
-        element: <FullCalender />,
-      }
-    ],
-  },
-  {
-    path: "/login",
-    element: <LoginForm />,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-  {
-    path: "/services",
-    element: <Services />,
-  },
-  {
-    path: "/homepage",
-    element: <HomePage />,
-  },
-  {
-    path: "/homepage2",
-    element: <HomePage2 />,
-  },
-  {
-    path: "/contact",
-    element: <ContactUs />,
-  },
-  
+export const routes = createBrowserRouter([{
+        path: "/",
+        element: < App / > ,
+        children: [{
+                path: "/viewPatients",
+                element: < ViewPatients / > ,
+            },
+            {
+                path: "/viewDoctors",
+                element: < ViewDoctors / > ,
+            },
+            {
+                path: "/fullCalender",
+                element: < FullCalender / > ,
+            },
+            {
+                path: "/viewDoctorAppointments",
+                element: < ViewDoctorAppointments / > ,
+            },
+            {
+                path: "/calendar",
+                element: < FullCalender / > ,
+            }
+        ],
+    },
+    // {
+    //   path: "/login",
+    //   element: <LoginForm />,
+    // },
+    // {
+    //   path: "/register",
+    //   element: <Register/>,
+    // },
+    {
+        path: "/services",
+        element: < Services / > ,
+    },
+    {
+        path: "/homepage",
+        element: < HomePage / > ,
+    },
+    {
+        path: "/homepage2",
+        element: < HomePage2 / > ,
+    },
+    {
+        path: "/contact",
+        element: < ContactUs / > ,
+    },
+
 ]);
