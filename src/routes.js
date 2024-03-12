fb6ed4fdd423c2a190e474e708dc796f03b49c98
SaraMohamedKamal <sarah.mohamed2070@gmail.com>
2024-03-12 04:07:10 +0200

    profilePage

--- src/routes.js
+++ src/routes.js
@@ -18,84 +18,84 @@ import Benefits from './components/PatientComponents/Benefits/Benefits.jsx'
 import DragDrop from "./components/PatientComponents/DragDrop/DragDrop.jsx";
 import DoctorsPreview from "./components/PatientComponents/DoctorsPreview/DoctorsPreview.jsx";
 import ResetPassword from "./shared/ResetPassword/ResetPassword.jsx";
-export const routes = createBrowserRouter([
-  {
-    path: "/admin",
-    element: <Admin />,
-    children: [
-      {
-        path: "/admin/viewPatients",
-        element: <ViewPatients />,
-      },
-      {
-        path: "/admin/viewDoctors",
-        element: <ViewDoctors />,
-      },
-      {
-        path: "/admin/calendar",
-        element: <FullCalender />,
-      }
-    ],
-  },
-  {
-    path: "/doctor",
-    element: <Doctor />,
-    children: [
-      {
-        path: "/doctor/viewDoctorAppointments",
-        element: <ViewDoctorAppointments />
-      },
-      {
-        path: "/doctor/viewDoctorSchedule",
-        element: <ViewDoctorSchedule />
-      }
-    ],
-  },
-  {
-    path: "/patient",
-    element: <Patient />,
-    children: [
-      {
-        path: "/patient/doctors-preview",
-        element: <DoctorsPreview />
-      },
-      {
-        path: "/patient/homepage",
-        element: <HomePage />
-      },
-      {
-        path: "/patient/contact",
-        element: <ContactUs />,
-      },
-      {
-        path: "/patient/services",
-        element: <Services />,
-      },
-      {
-        path: "/patient/homepage2",
-        element: <HomePage2 />,
-      },
-      {
-        path: "/patient/benefits",
-        element: <Benefits />,
-      },
-      {
-        path: "/patient/uploadXRay",
-        element: <DragDrop />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <LoginForm />,
-  },
-  {
-    path: "/resetpassword",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-  
+import ProfilePage from "./components/PatientComponents/ProfilePage/ProfilePage.jsx";
+export const routes = createBrowserRouter([{
+        path: "/admin",
+        element: < Admin / > ,
+        children: [{
+                path: "/admin/viewPatients",
+                element: < ViewPatients / > ,
+            },
+            {
+                path: "/admin/viewDoctors",
+                element: < ViewDoctors / > ,
+            },
+            {
+                path: "/admin/calendar",
+                element: < FullCalender / > ,
+            }
+        ],
+    },
+    {
+        path: "/doctor",
+        element: < Doctor / > ,
+        children: [{
+                path: "/doctor/viewDoctorAppointments",
+                element: < ViewDoctorAppointments / >
+            },
+            {
+                path: "/doctor/viewDoctorSchedule",
+                element: < ViewDoctorSchedule / >
+            }
+        ],
+    },
+    {
+        path: "/patient",
+        element: < Patient / > ,
+        children: [{
+                path: "/patient/doctors-preview",
+                element: < DoctorsPreview / >
+            },
+            {
+                path: "/patient/homepage",
+                element: < HomePage / >
+            },
+            {
+                path: "/patient/contact",
+                element: < ContactUs / > ,
+            },
+            {
+                path: "/patient/services",
+                element: < Services / > ,
+            },
+            {
+                path: "/patient/homepage2",
+                element: < HomePage2 / > ,
+            },
+            {
+                path: "/patient/benefits",
+                element: < Benefits / > ,
+            },
+            {
+                path: "/patient/uploadXRay",
+                element: < DragDrop / > ,
+            }, {
+                path: "/patient/profilePage",
+                element: < ProfilePage / > ,
+            }
+        ],
+    },
+    {
+        path: "/login",
+        element: < LoginForm / > ,
+    },
+    {
+        path: "/resetpassword",
+        element: < ResetPassword / > ,
+    },
+    {
+        path: "/register",
+        element: < Register / > ,
+    },
+
 ]);
