54dbd603843418f8bcc120bfde3e230f17d3d769
SabahHassan692 <103220823+SabahHassan692@users.noreply.github.com>
2024-04-25 11:16:19 +0200

    contactus responsiveness

--- src/components/PatientComponents/ContactUs/ContactUs.jsx
+++ src/components/PatientComponents/ContactUs/ContactUs.jsx
@@ -7,8 +7,11 @@ import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useMediaQuery } from '@mui/material';
 import contactImage from '../../../images/Sent Message-bro.png'
-
-
+import Form from "react-bootstrap/Form";
+import Input from '@mui/joy/Input';
+import { FaUser } from "react-icons/fa";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import './ContactUs.scss';
 const ContactUs = () => {
   const form = useRef();
   const sendEmail = (e) => {
@@ -32,47 +35,40 @@ const ContactUs = () => {
       );
   };
 
-  // Use Media Query hook to check screen size
-  const isMediumOrSmaller = useMediaQuery('(max-width: 960px)');
-
   return (
-    <Container>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12}>
+    <Container style={{width: '100%' , height: 'auto'}}>
+      <Grid container>
+        <Grid item xs={3} sm={3} md={6} lg={6} xl={6} >
           <Link className="back-to-home-from-contact" to={'/patient/homepage'}>
             <FaHome />
           </Link>
         </Grid>
-        {/* Conditionally render the image based on screen size */}
-        {!isMediumOrSmaller && (
-          <Grid item xs={12} md={6}>
-            <img src={contactImage} alt="contactus" style={{ width: '100%', height: 'auto' }} />
-          </Grid>
-        )}
-        <Grid item xs={12} md={6}>
-          <div className="cntct-form">
-            <h1 className="title">Contact us</h1>
-            <form className="contact-form" ref={form} onSubmit={sendEmail}>
-              {/* Your form content goes here */}
-              <Stack direction='column' spacing={2}>
-              <Stack direction='column' spacing={0.2}>
-                <label className="contact-label">Name</label>
-                <input className="contact-input" type="text" name="user_name" />
-              </Stack>
-              <Stack spacing={0.2} direction='column'>
-                <label className="contact-label">Email</label>
-                <input className="contact-input" type="email" name="user_email" />
-              </Stack>
-              <Stack spacing={0.2} direction='column'>
-                <label className="contact-label">Message</label>
-                <textarea name="message" />
-              </Stack>
-              <input className="submit-btn" type="submit" value="send message" />
-              </Stack>
-            </form>
-          </div>
+        <Grid item xs={9} sm={9} md={6} lg={6} xl={6} sx={{fontSize: {xs: '20px', sm: '27px', md:'27px',lg: '50px',xl: '50px'}}}>
+          <h1 className="title">Contact us</h1>
         </Grid>
       </Grid>
+      <Grid container >
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} sx={{ display: { xs: 'none', sm: 'none', md: 'block', lg: 'flex', xl: 'flex'} }}>
+          <img src={contactImage} alt="contactus" style={{ width: '100%', height: 'auto' }}/>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+        <Form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <Stack direction='column' spacing={3} className="profile-details-stack">
+            <Stack direction='column' spacing={0.2}>
+                <Input placeholder="Name" startDecorator={<FaUser />} variant="plain" color="neutral"/>
+            </Stack>
+            <Stack spacing={0.2} direction='column'>
+                <Input placeholder="Email" startDecorator={<MdOutlineAlternateEmail />} variant="plain" color="neutral" type="email"/>
+            </Stack>
+            <Stack spacing={0.2} direction='column'>
+                <Input placeholder="Email" startDecorator={<MdOutlineAlternateEmail />} variant="plain" color="neutral" type="email"/>
+            </Stack>
+            <input className="submit-btn" type="submit" value="send message" />
+          </Stack>
+        </Form>
+        </Grid>
+      </Grid>
+
     </Container>
   );
 };
