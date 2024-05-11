131b1f0a384ffbfb6833e9ee5d9a8592c100b6b0
SaraMohamedKamal <sarah.mohamed2070@gmail.com>
2024-05-11 20:05:31 +0300

    navbar

--- src/components/PatientComponents/Navbar/Navbar.jsx
+++ src/components/PatientComponents/Navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import { NavLink } from "react-router-dom";
 import { FiMenu } from "react-icons/fi";
-
 import axios from "axios";
 import {getAuthenticatedUser} from '../../../Helper/Storage'
 
@@ -12,18 +11,13 @@ const sabah = require('../../../images/saboha.jpeg');
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const userToken = getAuthenticatedUser();
-  const refreshToken = userToken.refreshToken;
-  
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth <= 768);
     };
-
     handleResize();
-
     window.addEventListener("resize", handleResize);
 
     return () => {
@@ -34,25 +28,25 @@ const Navbar = () => {
   const handleMenuClick = () => {
     setMenuOpen(!menuOpen);
   };
-
+  const userToken = getAuthenticatedUser();
+    const refreshToken = userToken.refreshToken;
+ 
   const [patientId, getPatientId] = useState(0);
   useEffect(() => {
-
       axios
       .get(`http://localhost:8070/user/getUserByToken/${refreshToken}`)
       .then((response) => {
-        getPatientId(response.data.id);
-        console.log(response)
+          getPatientId(response.data.id);
       })
       .catch((error) => {
-          console.log('you need to login or signup');
+          console.log(error);
       });
   }, []);
 
   return (
     <nav>
       <h1 className="web-header">orthopedista</h1>
-      <div className="menu" >
+      <div className="menu" onClick={handleMenuClick}>
         <FiMenu />   
       </div>
       <ul className={menuOpen ? "open" : ""}>
@@ -62,25 +56,22 @@ const Navbar = () => {
         <li>
           <NavLink to="/patient/uploadXRay">Upload X-ray</NavLink>
         </li>
-        {
-          patientId !== 0 ?
-            <div>
-              <NavLink to='/patientProfile'>
-                
-                  <Avatar alt="Sabah hassan" src={sabah} />
-              </NavLink>
-            </div>
-            :
-            <>
-              <li>
-                <NavLink to="/register">signup</NavLink>
-              </li>
-              <li>
-                <NavLink to="/login">login</NavLink>
-              </li>
-            </>
-        }
+        <li>
+          <NavLink to="/register">SignUp</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">login</NavLink>
+        </li>
 
+        <li>
+          <NavLink to='/patientProfile'>
+            {isSmallScreen ? (
+              <span>Profile</span>
+            ) : (
+              <Avatar alt="Sabah hassan" src={sabah} />
+            )}
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
