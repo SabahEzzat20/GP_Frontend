a83834026a181aec283db2bffeb63cf0a90e353d
SaraMohamedKamal <sarah.mohamed2070@gmail.com>
2024-05-12 15:59:44 +0300

    patientDetails for doctor changes

--- src/components/DoctorComponents/PatientDetailsForDoctor/PatientDetailsForDoctor.jsx
+++ src/components/DoctorComponents/PatientDetailsForDoctor/PatientDetailsForDoctor.jsx
@@ -6,23 +6,23 @@ import Tooltip from '@mui/material/Tooltip';
 import { VscClose } from "react-icons/vsc";
 import IconButton from '@mui/material/IconButton';
 import './PatientDetailsForDoctor.scss';
-const PatientDetailsForDoctor = ({patient}) => {
+const PatientDetailsForDoctor = ({doctorAppointment}) => {
     
     return (
         <div>
-            <div className="patient-identification" style={{backgroundColor: patient.status==="On Going" ? 'rgba(243, 252, 255, 0.986)' : 'transparent'}}>
+            <div className="patient-identification" style={{backgroundColor: doctorAppointment.status==="0" ? 'rgba(243, 252, 255, 0.986)' : 'transparent'}}>
                 <Stack direction="row" spacing={2}>
                     <Avatar alt={"sabah"} src={userAvatar} />
                     <div className="patient-info">
                         <Stack direction="column" spacing={0.1}>
-                            <p className='patient-name' style={{color: patient.status==="On Going" ? 'rgb(67, 155, 255)' : 'black'}}>{patient.name}</p>
-                            <p className='patient-date'>{patient.date}</p>
+                            <p className='patient-name' style={{color: doctorAppointment.status==="0" ? 'rgb(67, 155, 255)' : 'black'}}>{doctorAppointment.name}</p>
+                            <p className='patient-date'>{doctorAppointment.date}</p>
                         </Stack>
                     </div>
                 </Stack>
                 <Stack direction="row" spacing={2}>
                     <div className="last-of-stack">
-                        <div className="patient-status" style={{color: patient.status==="On Going" ? 'rgb(67, 155, 255)' : 'black'}}>{patient.status}</div>
+                        <div className="patient-status" style={{color: doctorAppointment.status==="0" ? 'rgb(67, 155, 255)' : 'black'}}>{doctorAppointment.status}</div>
                         <div className="cancel-appointment">
                             <Tooltip title="cancel appointment">
                                 <IconButton color="primary" size='small'>
